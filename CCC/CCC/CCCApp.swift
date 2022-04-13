f0acc84acebb5206926b6731b250ab91cba7de0d
Mohsin Ali Ayub <meismosin@gmail.com>
2022-04-13 10:30:59 +0200

    create project

---
+++ CCC/CCC/CCCApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCCApp.swift
+//  CCC
+//
+//  Created by Mohsin Ali Ayub on 13.04.22.
+//
+
+import SwiftUI
+
+@main
+struct CCCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
