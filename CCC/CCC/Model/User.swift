5d72ec3c970c3cf4534d0060f193548d6ad7a504
Mohsin Ali Ayub <meismosin@gmail.com>
2022-04-19 10:27:19 +0200

    add user model

---
+++ CCC/CCC/Model/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  CCC
+//
+//  Created by Mohsin Ali Ayub on 19.04.22.
+//
+
+import Foundation
+
+struct User {
+    
+    var email: String
+    var password: String
+    var firstName: String
+    var lastName: String
+    var phoneNumber: String
+    var profilePhotoUrlString: String
+    var isVerified = false
+    
+}
